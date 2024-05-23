3b58fdfb7b774f57dd28aaf8a64a976f0feb1e0d
deepspacesonja <75392165+BlurbyTheSlurp@users.noreply.github.com>
2024-05-23 15:22:39 +0200

    ....

--- js/app.js
+++ js/app.js
@@ -13,6 +13,21 @@
 // 3. Udskriv billeder i tilfældig rækkefølge.
 // 4. Når spillet er forbi, brug confetti.js til at vise confetti på skærmen. Mere info her : https://github.com/abelmoricz/abelmoricz.github.io/tree/9eac02160de7bb57170441a441db96b36e8341d8/confetti.js-master
 
+/*
+  For at aktivere et kort, tilføj en css class med navnet active
+  eks. object.classList.add("active"); . men husk også at fjerne class hvis 
+  kortet ikke matcher. .classList.remove("active");
+*/
+
+// Step 1. Tilføj click event på alle kort holder elemente <figure>.
+// Step 2. Tilføj check om 2 billeder som er aktive matcher.
+
+// Ekstra opgaver.
+// 1. Indbyg en score som give + point ved korret match, og - point ved forkert.
+// 2. Indbyg en reset knap så spillet kan genstrate.
+// 3. Udskriv billeder i tilfældig rækkefølge.
+// 4. Når spillet er forbi, brug confetti.js til at vise confetti på skærmen. Mere info her : https://github.com/abelmoricz/abelmoricz.github.io/tree/9eac02160de7bb57170441a441db96b36e8341d8/confetti.js-master
+
 let flipCard = document.querySelectorAll("figure.card");
 let firstClick = "";
 let score = 0;
