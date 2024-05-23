91f315a31c86fd01e611793edeb7a46bbbde0947
deepspacesonja <75392165+BlurbyTheSlurp@users.noreply.github.com>
2024-05-23 14:10:53 +0200

    ...

--- js/app.js
+++ js/app.js
@@ -11,8 +11,15 @@
 // 1. Indbyg en score som give + point ved korret match, og - point ved forkert.
 // 2. Indbyg en reset knap så spillet kan genstrate.
 // 3. Udskriv billeder i tilfældig rækkefølge.
-/*
-eks ved at bruge en array:
+// 4. Når spillet er forbi, brug confetti.js til at vise confetti på skærmen. Mere info her : https://github.com/abelmoricz/abelmoricz.github.io/tree/9eac02160de7bb57170441a441db96b36e8341d8/confetti.js-master
+
+let flipCard = document.querySelectorAll("figure.card");
+let firstClick = "";
+let score = 0;
+let scoreElement = document.querySelector("#score")
+
+let matches = 0;
+
 const memoryPictureUrls = [
   "https://picsum.photos/seed/memory_1/300/300",
   "https://picsum.photos/seed/memory_2/300/300",
@@ -27,5 +34,54 @@ const memoryPictureUrls = [
   "https://picsum.photos/seed/memory_5/300/300",
   "https://picsum.photos/seed/memory_6/300/300",
 ];
-*/
-// 4. Når spillet er forbi, brug confetti.js til at vise confetti på skærmen. Mere info her : https://github.com/abelmoricz/abelmoricz.github.io/tree/9eac02160de7bb57170441a441db96b36e8341d8/confetti.js-master
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+}
+
+function shuffle() {
+  let array = memoryPictureUrls
+  shuffleArray(array);
+  for (let index = 0; index < flipCard.length; index++) {
+    flipCard[index].children[0].scr = array[index]
+  }
+}
+shuffle()
+
+for (let index = 0; index < flipCard.length; index++){
+  flipCard[index].addEventListener("click", (data) => {
+    if (firstClick === "") {
+      firstClick = data.target;
+      data.target.classList.add("active");
+      } else (firstClick !== data.target) {
+        console.log(firstClick.children[0].scr)
+        console.log(data.target.children[0].scr)
+
+        if (firstClick.children[0].scr === data.target.children[0].scr) {
+          firstClick ="";
+          data.target.classList.add("active");
+          matches +=1;
+          score += 1;
+         if(matches == 6){
+          confetti.start()
+        } else{
+          firstClick.classList.remove("active");
+          firstClick ="";
+          score -= 1;
+        }
+    }
+    scoreElement.innerText = score;
+  });
+}
+
+function reset() {
+  for (let index = 0; index < flipCard.length; index++) {
+    flipCard[index].classList.remove("active");
+  }
+  score = 0;
+  scoreElement.innerText = score;
+  matches = 0;
+  confetti.stop();
+}
